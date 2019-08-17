6c717d58ae24a5867504ae8d23e4ee1abd8f767b
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2019-08-17 13:05:45 -0300

    Button is improved

---
+++ src/MaterialDesignControls/Implementations/MaterialFieldLayout.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Plugin.MaterialDesignControls.Implementations
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MaterialFieldLayout : StackLayout
+    {
+        public MaterialFieldLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
