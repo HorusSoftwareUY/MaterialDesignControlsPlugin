0ffb476d52feed15b42905b6a374902c249a53cf
Seba Cabrera <sebastian.cabrera@horus.com.uy>
2019-09-05 11:01:41 -0300

    Added VM for chips samples

---
+++ example/ExampleMaterialDesignControls/ViewModels/MaterialChipViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ExampleMaterialDesignControls.ViewModels
+{
+    public class MaterialEntryViewModel : BaseViewModel
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private string nameError;
+
+        public string NameError
+        {
+            get { return nameError; }
+            set { SetProperty(ref nameError, value); }
+        }
+
+        public delegate Task DisplayAlertType(string title, string message, string cancel);
+
+        public DisplayAlertType DisplayAlert { get; set; }
+
+        public ICommand TapCommand => new Command(OnTapCommand);
+
+        private async void OnTapCommand()
+        {
+            if (!string.IsNullOrEmpty(this.Name))
+            {
+                this.NameError = null;
+                await this.DisplayAlert.Invoke("", "Saved", "Ok");
+            }
+            else
+            {
+                this.NameError = "The message is required";
+            }
+        }
+    }
+}
