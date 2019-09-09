8a82fe55bab48e002ea57b5a42183ca119fda544
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2019-09-09 16:32:27 -0300

    Fix the IsEnabled error on MaterialButton

--- src/MaterialDesignControls/Implementations/CustomButton.cs
+++ src/MaterialDesignControls/Implementations/CustomButton.cs
@@ -1,8 +1,18 @@
-﻿using Xamarin.Forms;
+﻿using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace Plugin.MaterialDesignControls.Implementations
 {
     public class CustomButton : Button
     {
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName.Equals(IsEnabledProperty.PropertyName) && !this.IsEnabled)
+            {
+                this.IsEnabled = true;
+            }
+        }
     }
 }
