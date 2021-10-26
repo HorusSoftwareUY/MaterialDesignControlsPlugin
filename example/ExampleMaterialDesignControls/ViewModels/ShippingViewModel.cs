9cf69c7543cf907f44c67ef28424a2a51a0ce32f
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2021-10-26 17:10:16 -0300

    Feature/design xaml (#32)
    * Material Control Page
    
    * Material Button Page
    
    * Material Chips Page
    
    * Material Date Picker
    
    * Material Editor Page
    
    * Material Entry Page
    
    * Material Code Entry
    
    * Material Picker Page
    
    * Material Time Page
    
    * Material Slider Page
    
    * update scroll bar visibility
    
    * update icons and colors
    
    * Update
    
    * Update images
    
    * update splash
    
    * Update splash and assets
    
    * Update name and code
    
    * Update app name
    
    * Update images
    
    * update
    
    * update readme
    
    * update readme
    
    * update readme
    
    * Update readme
    
    * update readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * update readme
    
    * Update Readme
    
    * Fix icon issue
    
    * Upload assets
    
    * upload gift
    
    Co-authored-by: AntonioBonillaHorus <antoniobonilla@horus.com.uy>

---
+++ example/ExampleMaterialDesignControls/ViewModels/ShippingViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ExampleMaterialDesignControls.ViewModels
+{
+    public class ShippingViewModel : BaseViewModel
+    {
+        private bool isEnabled;
+
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set { SetProperty(ref isEnabled, value); }
+        }
+
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        private bool cancelIsVisible = true;
+
+        public bool CancelIsVisible
+        {
+            get { return cancelIsVisible; }
+            set { SetProperty(ref cancelIsVisible, value); }
+        }
+
+        private bool deleteIsVisible = false;
+
+        public bool DeleteIsVisible
+        {
+            get { return deleteIsVisible; }
+            set { SetProperty(ref deleteIsVisible, value); }
+        }
+
+        public ICommand TapCommand => new Command<string>(OnTap);
+
+        public ICommand TapCommand2 => new Command<string>(OnTap2, (var) => { return IsEnabled; });
+
+        public async void OnTap(object parameter)
+        {
+            if (parameter != null)
+            {
+                if (parameter is string && ((string)parameter).ToString().Equals("Saved"))
+                {
+                    this.IsBusy = true;
+                    await Task.Delay(2000);
+                    this.IsBusy = false;
+
+                    this.IsEnabled = true;
+                }
+
+                string text = parameter.ToString();
+                await this.DisplayAlert("", text, "Ok");
+            }
+        }
+
+        public async void OnTap2(object parameter)
+        {
+            if (parameter != null)
+            {
+                string text = parameter.ToString();
+                await this.DisplayAlert("", text, "Ok");
+
+                this.CancelIsVisible = false;
+                this.DeleteIsVisible = true;
+            }
+        }
+    }
+}
