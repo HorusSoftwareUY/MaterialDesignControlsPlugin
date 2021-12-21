65f905266c8b72e30811478fba304e37a7858257
carlin yahuira <car.achahui@hotmail.com>
2021-12-21 11:21:31 -0500

    testing

---
+++ src/MaterialDesignControls/Controls/MaterialSegmented.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Plugin.MaterialDesignControls
+{
+    public partial class MaterialSegmented : ContentView
+    {
+        public MaterialSegmented()
+        {
+            InitializeComponent();
+        }
+    }
+}
