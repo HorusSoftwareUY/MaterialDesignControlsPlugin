e59001791b5f461030f4721e344407a923494649
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2022-09-16 21:47:27 -0300

    Disable dark mode

--- example/ExampleMaterialDesignControls/App.xaml.cs
+++ example/ExampleMaterialDesignControls/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -14,21 +12,6 @@ namespace ExampleMaterialDesignControls
             InitializeComponent();
 
             MainPage = new MainPage();
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
         }
     }
 }
--- example/ExampleMaterialDesignControls/MainPage.xaml.cs
+++ example/ExampleMaterialDesignControls/MainPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
-using ExampleMaterialDesignControls.Pages;
+﻿using ExampleMaterialDesignControls.Pages;
 using Xamarin.Forms;
 
 namespace ExampleMaterialDesignControls
@@ -18,19 +12,6 @@ namespace ExampleMaterialDesignControls
             this.Children.Add(new ProductDetailsPage());
             this.Children.Add(new AddCardPage());
             this.Children.Add(new ShippingPage());
-
-            //masterPage.ListView.ItemSelected += OnItemSelected;
         }
-
-        //void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
-        //{
-        //    var item = e.SelectedItem as MasterPageItem;
-        //    if (item != null)
-        //    {
-        //        Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
-        //        masterPage.ListView.SelectedItem = null;
-        //        IsPresented = false;
-        //    }
-        //}
     }
 }
