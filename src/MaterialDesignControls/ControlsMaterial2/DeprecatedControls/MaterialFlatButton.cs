4fa6565099b1ffbc2840c28bec48f8f1d5255e6a
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2022-09-16 23:36:43 -0300

    Add compatibility to old MaterialButtons

---
+++ src/MaterialDesignControls/ControlsMaterial2/DeprecatedControls/MaterialFlatButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Plugin.MaterialDesignControls
+{
+    [Obsolete("MaterialFlatButton is deprecated, please use MaterialButton of Material 3 instead with type Text.")]
+    public class MaterialFlatButton : MaterialButton
+    {
+        public MaterialFlatButton()
+        {
+            this.frmLayout.BackgroundColor = Color.Transparent;
+            this.frmLayout.BorderColor = Color.Transparent;
+        }
+    }
+}
---
+++ src/MaterialDesignControls/ControlsMaterial2/DeprecatedControls/MaterialOutlineButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Plugin.MaterialDesignControls
+{
+    [Obsolete("MaterialOutlineButton is deprecated, please use MaterialButton of Material 3 instead with type Outlined.")]
+    public class MaterialOutlineButton : MaterialButton
+    {
+        public MaterialOutlineButton()
+        {
+            this.frmLayout.BackgroundColor = Color.Transparent;
+        }
+    }
+}
