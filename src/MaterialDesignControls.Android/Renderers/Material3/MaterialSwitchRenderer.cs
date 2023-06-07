f000b005d6e75401e246ba143a4b752771d839e3
AndresAldana <andres.aldana@horus.com.uy>
2023-06-07 15:04:18 -0500

    feat: renderesrs

---
+++ src/MaterialDesignControls.Android/Renderers/Material3/MaterialSwitchRenderer.cs
@@ -0,0 +1,95 @@
+﻿using Android.Runtime;
+using Android.Views.Accessibility;
+using Android.Views;
+using Java.Lang;
+using Plugin.MaterialDesignControls.Material3;
+using Plugin.MaterialDesignControls.Material3.Android;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Content;
+using Switch = Android.Widget.Switch;
+
+[assembly: ExportRenderer(typeof(MaterialSwitch), typeof(MaterialSwitchRenderer))]
+
+namespace Plugin.MaterialDesignControls.Material3.Android
+{
+    public class MaterialSwitchRenderer : VisualElementRenderer<ContentView>
+    {
+        public static void Init() { }
+
+
+        private readonly Switch _a11YSwitch;
+
+        public MaterialSwitchRenderer(Context context) : base(context)
+        {
+            _a11YSwitch = new Switch(context);
+        }
+
+        /// <inheritdoc />
+        protected override void OnElementChanged(ElementChangedEventArgs<ContentView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement == null)
+            {
+                return;
+            }
+
+            MaterialSwitch customSwitch = e.NewElement as MaterialSwitch;
+
+            _a11YSwitch.Checked = customSwitch.IsToggled;
+
+        }
+
+        /// <inheritdoc />
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName.Equals(nameof(MaterialSwitch.IsToggled)))
+            {
+                MaterialSwitch customSwitch = sender as MaterialSwitch;
+
+                if (_a11YSwitch.Checked != customSwitch.IsToggled)
+                {
+                    _a11YSwitch.Checked = customSwitch.IsToggled;
+                    AnnounceForAccessibility(GetStateDescription());
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public override ICharSequence? AccessibilityClassNameFormatted => new String(_a11YSwitch.Class.Name);
+
+        /// <inheritdoc />
+        public override void OnInitializeAccessibilityNodeInfo(AccessibilityNodeInfo? info)
+        {
+            if (info != null)
+            {
+                info.Checkable = true;
+                info.Checked = _a11YSwitch.Checked;
+                info.Text = GetStateDescription();
+            }
+
+            base.OnInitializeAccessibilityNodeInfo(info);
+        }
+
+
+        private string GetStateDescription()
+        {
+            return _a11YSwitch.Checked ? _a11YSwitch.TextOn : _a11YSwitch.TextOff;
+        }
+
+        public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Space || keyCode == Keycode.Enter)
+            {
+                MaterialSwitch customSwitch = Element as MaterialSwitch;
+                customSwitch.IsToggled = !customSwitch.IsToggled;
+            }
+
+            return base.OnKeyUp(keyCode, e);
+        }
+    }
+}
---
+++ src/MaterialDesignControls.iOS/Renderers/Material3/MaterialSwitchRenderer.cs
@@ -0,0 +1,53 @@
+﻿using Plugin.MaterialDesignControls.Material3;
+using Plugin.MaterialDesignControls.Material3.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MaterialSwitch), typeof(MaterialSwitchRenderer))]
+
+
+namespace Plugin.MaterialDesignControls.Material3.iOS
+{
+    internal class MaterialSwitchRenderer : VisualElementRenderer<ContentView>
+    {
+        public new static void Init()
+        {
+        }
+
+        private readonly UISwitch _a11YSwitch = new UISwitch();
+
+        /// <inheritdoc />
+        protected override void OnElementChanged(ElementChangedEventArgs<ContentView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                _a11YSwitch.On = ((MaterialSwitch)e.NewElement).IsToggled;
+            }
+
+        }
+
+        /// <inheritdoc />
+        public override string AccessibilityValue
+        {
+            get => _a11YSwitch.On ? "1" : "0";
+            set { }
+        }
+
+        /// <inheritdoc />
+        public override UIAccessibilityTrait AccessibilityTraits
+        {
+            get => _a11YSwitch.AccessibilityTraits;
+            set { }
+        }
+
+        /// <inheritdoc />
+        public override bool AccessibilityActivate()
+        {
+            _a11YSwitch.SetState(!_a11YSwitch.On, false);
+            return base.AccessibilityActivate();
+        }
+    }
+}
