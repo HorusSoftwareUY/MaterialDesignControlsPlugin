050e5ebb6efd9c20b96c321dd12e34bae211543f
Andrés Aldana <83594458+agaldanaw2@users.noreply.github.com>
2023-08-10 09:08:19 -0500

    MaterialChips migration to v3 (#131)
    * first: approximation materialchipsgroup
    
    * chore: isSelected binding two way
    
    * add issues
    
    * chore: save current state
    
    * firs examples
    
    * test
    
    * feat: style , samples and documentation
    
    * Apply changes on styles
    
    * fix: some comments
    
    * Fix animation issue and modify the documentation
    
    * chore: add documentation
    
    * Update MaterialChipsGroup.md
    
    ---------
    
    Co-authored-by: andres <agaldanaw@unal.edu.co>
    Co-authored-by: Agustin Bonilla <agustin.bonilla@horus.com.uy>

--- src/MaterialDesignControls/Styles/DefaultStyles.cs
+++ src/MaterialDesignControls/Styles/DefaultStyles.cs
@@ -4,23 +4,29 @@ namespace Plugin.MaterialDesignControls.Styles
 {
     public static class DefaultStyles
     {
-        public static Color PrimaryColor { get; set; } = Color.FromHex("#2e85cc");
+        public static Color PrimaryColor { get; set; } = Color.FromHex("#6750A4");
 
-        public static Color PrimaryContainerColor { get; set; } = Color.FromHex("#eaf3fa");
+        public static Color OnPrimaryColor { get; set; } = Color.FromHex("#FFFFFF");
 
-        public static Color BackgroundColor { get; set; } = Color.White;
+        public static Color PrimaryContainerColor { get; set; } = Color.FromHex("#EADDFF");
 
-        public static Color TextColor { get; set; } = Color.Black;
+        public static Color OnPrimaryContainerColor { get; set; } = Color.FromHex("#21005D");
 
-        public static Color DisableColor { get; set; } = Color.FromHex("#808080");
+        public static Color TextColor { get; set; } = Color.FromHex("#1D1B20");
 
-        public static Color DisableContainerColor { get; set; } = Color.FromHex("#f2f2f2");
+        public static Color ErrorColor { get; set; } = Color.FromHex("B3261E");
+
+        public static Color DisableColor { get; set; } = Color.FromHex("#9791A1");
+
+        public static Color DisableContainerColor { get; set; } = Color.FromHex("#E5E3E8");
 
         public static Color OutlineVariantColor { get; set; } = Color.FromHex("#CAC4D0");
 
-        public static double ButtonAnimationParameter { get; set; } = 0.7;
+        public static double AnimationParameter { get; set; } = 0.7;
 
-        public static AnimationTypes ButtonAnimation { get; set; } = AnimationTypes.Fade;
+        public static AnimationTypes AnimationType { get; set; } = AnimationTypes.Fade;
+
+        public static bool AnimateError { get; set; } = true;
 
         public static string FontFamily { get; set; } = null;
 
