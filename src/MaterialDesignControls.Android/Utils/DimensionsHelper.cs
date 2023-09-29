8da39b4bc52c3abd2b63c44d50ec234dbf19ac19
Pablo González <pablo.gonzalez@horus.com.uy>
2023-09-29 09:51:49 -0300

    Fix/slider (#163)
    * Fix thumb image on iOS
    
    * Fix progress color on Android and spacing on both platforms
    
    * Fix padding on Android API < 23
    
    * Removed fixed height

---
+++ src/MaterialDesignControls.Android/Utils/DimensionsHelper.cs
@@ -0,0 +1,20 @@
+﻿using Android.Content;
+using Android.Util;
+
+namespace System
+{
+	public static class DimensionsHelper
+	{
+        public static float DpToPixels(this float valueInDp, Context context)
+        {
+            DisplayMetrics metrics = context.Resources.DisplayMetrics;
+            return TypedValue.ApplyDimension(ComplexUnitType.Dip, valueInDp, metrics);
+        }
+
+        public static int DpToPixels(this int valueInDp, Context context)
+        {
+            return Convert.ToInt32(((float)valueInDp).DpToPixels(context));
+        }
+    }
+}
+
